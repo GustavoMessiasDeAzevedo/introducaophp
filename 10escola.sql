4434d0e28c511dcb3dde018113a8f3d6d6ba9e6b
Gustavo Messias de Azevedo <gustavo.mazevedo1@senacsp.edu.br>
2026-03-06 19:21:27 -0300

    UC10 - Atividade prática - Criação banco de dados

---
+++ 10escola.sql
@@ -0,0 +1,14 @@
+CREATE TABLE escola_gustavo
+
+USE escola_gustavo;
+
+CREATE TABLE alunos (
+
+    id INT PRIMARY KEY IDENTITY,
+    nome VARCHAR(100) NOT NULL,
+    idade INT NOT NULL,
+    uf VARCHAR(2) NOT NULL,
+    cidade VARCHAR(60) NOT NULL
+);
+
+SELECT* FROM alunos;
